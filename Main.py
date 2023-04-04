60b2fa024c40cee7aad64071ebdcf2b17bd28e12
And5reas <and5reas.porcel@hotmail.com>
2023-04-04 13:45:45 -0300

    Changed resolution

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 window = Tk()  # Estanciando a janela
 
-window.geometry('1440x900')  # Definindo o tamanho da janela para 1440 por 900px
+window.geometry('1280x720')  # Definindo o tamanho da janela para 1280 por 720px
 window.title('MoreLife')  # Definindo o título da janela
 
 window.mainloop()  # Exibir a janela em um loop
