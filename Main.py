f4c2a217b0df730ff2c67576eb032590f85e4e39
And5reas <and5reas.porcel@hotmail.com>
2023-04-04 13:35:47 -0300

    Creating the basic of this program

--- Main.py
+++ Main.py
@@ -1,5 +1,8 @@
 from tkinter import *
 
-window = Tk()
+window = Tk()  # Estanciando a janela
 
-window.mainloop()
+window.geometry('1440x900')  # Definindo a tamanho da janela para 1440 por 900px
+window.title('MoreLife')  # Definindo a título da janela
+
+window.mainloop()  # Exibir a janela em um loop
