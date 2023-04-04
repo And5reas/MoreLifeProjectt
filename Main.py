a8d2b5f37a0f7eebc578b89be583229f33297032
And5reas <and5reas.porcel@hotmail.com>
2023-04-04 13:47:19 -0300

    Mudando comentário do window.mainloop()

--- Main.py
+++ Main.py
@@ -5,4 +5,4 @@ window = Tk()  # Estanciando a janela
 window.geometry('1280x720')  # Definindo o tamanho da janela para 1280 por 720px
 window.title('MoreLife')  # Definindo o título da janela
 
-window.mainloop()  # Exibir a janela em um loop
+window.mainloop()  # Exibir a janela no PC, manter ela alerta para eventos
