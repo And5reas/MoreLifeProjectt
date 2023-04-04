a70f967778acf1987d7b1b78955a4b5f5687243e
And5reas <and5reas.porcel@hotmail.com>
2023-04-04 13:37:16 -0300

    Corrigindo erros de português '-'

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 window = Tk()  # Estanciando a janela
 
-window.geometry('1440x900')  # Definindo a tamanho da janela para 1440 por 900px
-window.title('MoreLife')  # Definindo a título da janela
+window.geometry('1440x900')  # Definindo o tamanho da janela para 1440 por 900px
+window.title('MoreLife')  # Definindo o título da janela
 
 window.mainloop()  # Exibir a janela em um loop
