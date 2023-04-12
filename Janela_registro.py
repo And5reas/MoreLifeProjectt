2bc4b6584730b0a36ad7e8b241017fa7b090d7ba
And5reas <and5reas.porcel@hotmail.com>
2023-04-12 18:07:12 -0300

    Pequenas mudanças no layout, compo senha e email, ajustado.

--- Janela_registro.py
+++ Janela_registro.py
@@ -9,17 +9,17 @@ def main_screen():
 
     lbl_login_title = Label(text="Login", font=("Arial", 13), width=6, height=2, relief=FLAT)
     lbl_email_ori = Label(text="Email: ")
-    ent_email = Entry(font=("Arial", 13))
+    ent_email = Entry(font=("Arial", 11), width=27)
     lbl_password_ori = Label(text="Senha: ")
-    ent_password = Entry(font=("Arial", 13))
+    ent_password = Entry(font=("Arial", 11), width=27,  show='*')
     btn_login = Button(text="Login", width=10)
     btn_register = Button(text="Registrar-se", width=10)
 
     lbl_login_title.place(relx=0.5, y=20, anchor=CENTER)
-    lbl_email_ori.place(x=20, y=50)
-    ent_email.place(x=65, y=50)
-    lbl_password_ori.place(x=20, y=80)
-    ent_password.place(x=65, y=80)
+    lbl_email_ori.place(x=10, y=50)
+    ent_email.place(x=50, y=50)
+    lbl_password_ori.place(x=10, y=80)
+    ent_password.place(x=50, y=80)
     btn_login.place(relx=0.5, y=150, anchor=CENTER)
     btn_register.place(relx=0.5, y=190, anchor=CENTER)
 
