af02972a4ccca5f790db40433b548bf6b0872475
And5reas <and5reas.porcel@hotmail.com>
2023-04-15 15:39:56 -0300

    Feita interação com os botões

--- Janela_registro.py
+++ Janela_registro.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from kivy.config import Config
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 Builder.load_file('Resources/Janelas/login.kv')  # "Chamar" o arquivo kivy
 Config.set('graphics', 'resizable', '0')  # Configurar a janela para não poder ser redimensionada
@@ -9,8 +10,38 @@ Config.set('graphics', 'width', '420')  # "Setar" a largura da janela
 Config.set('graphics', 'height', '350')  # "Setar" a altura da janela
 
 
-class MyLayout(Widget):
+# Dedinir outra tela
+class JanelaLogin(Screen):
     pass
+
+
+class JanelaRegistrar(Screen):
+    pass
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
+class MyLayout(Widget):
+    def btn_login(self):
+        user_email = self.ids.email_input.text
+        user_password = self.ids.password_input.text
+
+        if "@" in user_email:
+            self.ids.error_login.text = ""
+            nome_user_email, domain_email = user_email.split("@")
+            print(f"Nome do email: {nome_user_email}\nServidor: {domain_email}")
+            print(f"Email: {user_email}\nSenha: {user_password}")
+        else:
+            self.ids.error_login.text = "Email e/ou senha inválido(s)"
+
+        # Limpar TextInputs
+        self.ids.email_input.text = ""
+        self.ids.password_input.text = ""
+
+    def btn_register(self):
+        print("Ainda não funciona '-'")
 
 
 class Login(App):
