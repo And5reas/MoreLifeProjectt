11d993d69674267dc7261641a98d9a7f027d992a
And5reas <and5reas.porcel@hotmail.com>
2023-04-15 16:41:23 -0300

    Excluindo conteúdo inútil

--- Janela_principal.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-
-window = Tk()  # Estanciando a janela
-
-window.geometry('1150x600')  # Definindo o tamanho da janela para 1280 por 720px
-window.title('MoreLife')  # Definindo o título da janela
-
-
-window.mainloop()  # Exibir a janela no PC, manter ela alerta para eventos
--- Main.py
+++ Main.py
@@ -1,8 +1,1 @@
-from tkinter import *
 
-window = Tk()  # Estanciando a janela
-
-window.geometry('1280x720')  # Definindo o tamanho da janela para 1280 por 720px
-window.title('MoreLife')  # Definindo o título da janela
-
-window.mainloop()  # Exibir a janela no PC, manter ela alerta para eventos
