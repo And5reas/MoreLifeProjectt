9d6036e7dbf83f5636827ecaf21c1875e00d8836
And5reas <and5reas.porcel@hotmail.com>
2023-04-15 14:22:26 -0300

    Add Aulas de kivy

--- LearningMoreKivy/Aula08 - BoxLayout.py
+++ LearningMoreKivy/Aula08 - BoxLayout.py
@@ -1,18 +1,23 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
+from kivy.core.window import Window
 
-Builder.load_file('KV files/label_color.kv')
+# Set the app size
+Window.size = (500, 700)
+
+# Point de .kv file Aula08 - 15
+Builder.load_file('KV files/flot.kv')
 
 
 class MyLayout(Widget):
     pass
 
 
-class AwsomeApp(App):
+class CalculatorApp(App):
     def build(self):
         return MyLayout()
 
 
 if __name__ == '__main__':
-    AwsomeApp().run()
+    CalculatorApp().run()
