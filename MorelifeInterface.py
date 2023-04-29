ec43a4110974e7a57cd8c35011a23350e7ac0252
And5reas <and5reas.porcel@hotmail.com>
2023-04-29 11:02:04 -0300

    Melhorando um pouco a interface main

--- MorelifeInterface.py
+++ MorelifeInterface.py
@@ -76,10 +76,10 @@ class JanelaMain(Screen):
         if self.ids.tgb_start_read_beats.state == 'down':
             ard_comunic_thread.event.set()
             ard_comunic_thread.iniciar(self.ids.Batimentos)
-            self.ids.tgb_start_read_beats.text = 'ON'
+            self.ids.tgb_start_read_beats.text = 'Parar'
         if self.ids.tgb_start_read_beats.state == 'normal':
             ard_comunic_thread.event.clear()
-            self.ids.tgb_start_read_beats.text = 'OFF'
+            self.ids.tgb_start_read_beats.text = 'Começar'
 
 
 class JanelaReport(Screen):
